68ede18637c9cb70402de16d904cefc2f1b08d49
lcfsbois <contactolcfs@tutamail.com>
2026-05-24 22:12:27 +0000

    Add files via upload

--- server.js
+++ server.js
@@ -1,16 +1,20 @@
-SERVER.JS
-
 const express = require('express');
+const cors = require('cors');
 const Gun = require('gun');
 require('gun-mongo-keyvalue'); 
 
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.use(cors({
+ origin: '*',
+ credentials: true,
+})); 
+
 app.use(Gun.serve);
 
 const server = app.listen(port, () => {
-  console.log(`Relay Gun con MongoDB corriendo en el puerto ${port}`);
+  console.log(`Servidor escuchando en el puerto ${port}`);
 });
 
 const gun = Gun({ 
